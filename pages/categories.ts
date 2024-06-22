6e33edb362656b66ed44f8a758510e1727a2b9f7
Jonathon Choo <jonno.choo@gmail.com>
2024-06-22 19:29:31 +1000

    fix: casing

--- pages/categories.ts
+++ pages/categories.ts
@@ -197,7 +197,7 @@ export const categories = [
         ],
     },
     {
-        category: 'animals',
+        category: 'Animals',
         words: [
             'alligator',
             'armadillo',
