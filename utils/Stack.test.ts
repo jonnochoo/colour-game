aa2f158272e443494aacec357fd056e5b373c708
Jonathon Choo <jonno.choo@gmail.com>
2024-06-23 08:18:05 +1000

    fix: broken test

--- utils/Stack.test.ts
+++ utils/Stack.test.ts
@@ -4,21 +4,21 @@ import { BlueBall } from './BlueBall'
 import { PurpleBall } from './PurpleBall'
 
 test('empty stack should not be full AND incomplete', () => {
-    const sut = new Stack([])
+    const sut = new Stack('1', [])
 
     expect(sut.isFull()).toBe(false)
     expect(sut.isComplete()).toBe(false)
 })
 
 test('one ball stack should not be full AND incomplete', () => {
-    const sut = new Stack([new BlueBall()])
+    const sut = new Stack('1', [new BlueBall()])
 
     expect(sut.isFull()).toBe(false)
     expect(sut.isComplete()).toBe(false)
 })
 
 test('five ball stack should be full AND incomplete', () => {
-    const sut = new Stack([
+    const sut = new Stack('1', [
         new PurpleBall(),
         new BlueBall(),
         new BlueBall(),
@@ -31,7 +31,7 @@ test('five ball stack should be full AND incomplete', () => {
 })
 
 test('five red ball stack should be full AND complete', () => {
-    const sut = new Stack([
+    const sut = new Stack('1', [
         new BlueBall(),
         new BlueBall(),
         new BlueBall(),
