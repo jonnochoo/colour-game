69c0390dd181dabb8e50ccebfea3a0c814fdefe6
Jonathon Choo <jonno.choo@gmail.com>
2024-06-29 20:03:42 +1000

    feat: add Zod

--- server/api/verse.ts
+++ server/api/verse.ts
@@ -1,5 +1,24 @@
 import axios from 'axios'
 import { removeUnnecessaryNewLine } from '../../utils/text/bible'
+import { z } from 'zod'
+
+const schema = z.object({
+    query: z.string(),
+    canonical: z.string(),
+    parsed: z.array(z.array(z.number())),
+    passage_meta: z.array(
+        z.object({
+            canonical: z.string(),
+            chapter_start: z.array(z.number()),
+            chapter_end: z.array(z.number()),
+            prev_verse: z.number(),
+            next_verse: z.number(),
+            prev_chapter: z.array(z.number()),
+            next_chapter: z.array(z.number()),
+        })
+    ),
+    passages: z.array(z.string()),
+})
 
 export default defineEventHandler(async (event) => {
     assertMethod(event, 'GET')
@@ -11,12 +30,12 @@ export default defineEventHandler(async (event) => {
         `https://api.esv.org/v3/passage/text/?q=${passageSearch}`,
         { headers: { Authorization: `Token ${apiSecret}` } }
     )
+    const passageResult = schema.parse(result.data)
     return {
-        ...result.data,
-        query,
+        ...passageResult,
         passageChunks: parseText(
             result.data.passages
-                ? removeUnnecessaryNewLine(result.data.passages[0])
+                ? removeUnnecessaryNewLine(passageResult.passages[0])
                 : ''
         ),
     }
