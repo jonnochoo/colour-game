2c1224080dd69e723e85c561cb7c7a0cd9657276
Jonathon Choo <jonno.choo@gmail.com>
2024-07-15 22:51:31 +1000

    feat: add bible verse fallback

--- components/BibleDashGrid.vue
+++ components/BibleDashGrid.vue
@@ -1,6 +1,12 @@
 <template>
     <DashGrid class="col-span-3">
-        <p class="border-l-4 border-[#ffd38f] pl-4 text-2xl">
+        <p v-if="error" class="border-l-4 border-[#ffd38f] pl-4 text-2xl">
+            Therefore, get rid of all moral filth and the evil that is so
+            prevalent and humbly accept the word planted in you, which can save
+            you.
+            <span class="mt-4 block font-bold">James 1:21</span>
+        </p>
+        <p v-else class="border-l-4 border-[#ffd38f] pl-4 text-2xl">
             {{ data.text }}
             <span class="mt-4 block font-bold">{{ data.verse }}</span>
         </p>
--- components/DashGrid.vue
+++ components/DashGrid.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="rounded-2xl bg-gradient-to-b from-[#312549] to-[#3e2f5b] p-8 font-[lexend] text-white"
+        class="rounded-2xl bg-gradient-to-b from-[#3e2f5b] to-[#3e2f5b] p-8 font-[lexend] text-white"
         :class="props.class"
     >
         <slot />
