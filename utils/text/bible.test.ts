bc2437f984c9e877ec19c01af9ca94a265d5c5ce
Jonathon Choo <jonno.choo@gmail.com>
2024-07-18 08:01:59 +1000

    fix: broken unit test

--- utils/text/bible.test.ts
+++ utils/text/bible.test.ts
@@ -26,7 +26,7 @@ test('parse splitPassageAndFootNote', () => {
         'John 3:15–16\n\n  [15] that whoever believes in him may have eternal life.(1)\n\nFor God So Loved the World\n\n  [16] “For God so loved the world,(2) that he gave his only Son, that whoever believes in him should not perish but have eternal life.\n\nFootnotes\n\n(1) 3:15 Some interpreters hold that the quotation ends at verse 15\n\n(2) 3:16 Or *For this is how God loved the world*\n (ESV)'
     const actual = splitPassageAndFootNote(test)
     expect(actual?.content).toStrictEqual([
-        'John 3:15–16\n\n  ',
+        '  ',
         '[15]',
         ' that whoever believes in him may have eternal life.(1)\n\nFor God So Loved the World\n\n  ',
         '[16]',
