6df79a5610c19385fcf4fdb9b2bede2a60fb41aa
Jonathon Choo <jonno.choo@gmail.com>
2024-07-18 10:16:31 +1000

    feat: update goals

--- components/Grid/Goals.vue
+++ components/Grid/Goals.vue
@@ -3,7 +3,7 @@
         <p class="mb-4 text-4xl font-bold text-[#fceb3c]">Goals</p>
         <ul class="lg:text-3xl">
             <li
-                class="mb-2 ml-6 list-disc"
+                class="mb-3"
                 v-for="goal in goals"
                 :class="{ 'text-gray-500 line-through': goal.isCompleted }"
             >
@@ -19,11 +19,12 @@ type goal = {
     isCompleted: boolean
 }
 const goals = ref<goal[]>([
-    { name: '🌲Organise Arborist', isCompleted: false },
-    { name: '✈️Start next steps for Cambodia', isCompleted: false },
-    { name: '🍿Go to the cinemas as a family', isCompleted: false },
-    { name: '🚶Go on a family bushwalk', isCompleted: false },
-    { name: '🐒Visit Sydney Zoo', isCompleted: true },
-    { name: "🥳Celebrate Abi's party", isCompleted: true },
+    { name: '🌲 Organise Arborist', isCompleted: false },
+    { name: '✈️ Start next steps for Cambodia', isCompleted: false },
+    { name: '🍿 Go to the cinemas as a family', isCompleted: false },
+    { name: '🚶 Go on a family bush walk', isCompleted: false },
+    { name: '💌 Write message to Nai Nai', isCompleted: false },
+    { name: '🐒 Visit Sydney Zoo', isCompleted: true },
+    { name: "🥳 Celebrate Abi's party", isCompleted: true },
 ])
 </script>
