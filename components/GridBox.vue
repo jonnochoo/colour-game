a134258ba27f6d615554ee4d4a90ed552f2b8f8b
Jonathon Choo <jonno.choo@gmail.com>
2024-07-18 15:53:11 +1000

    feat: change opacity on home page

--- components/GridBox.vue
+++ components/GridBox.vue
@@ -1,7 +1,7 @@
 <template>
     <RouterLink
         :to="to"
-        class="rounded-2xl bg-gradient-to-b from-[#3e2f5b] to-[#3e2f5b] p-10 text-center font-[lexend] text-4xl text-white"
+        class="rounded-2xl bg-gradient-to-b from-[#3e2f5b] to-[#3e2f5b] p-10 text-center font-[lexend] text-4xl text-white opacity-85"
     >
         {{ props.text }}
     </RouterLink>
