97ab00b5c7a21d3d6f8fcafb61cfb9d62911723e
Jonathon Choo <jonno.choo@gmail.com>
2024-07-18 16:03:36 +1000

    feat: update

--- components/Grid/Events.vue
+++ components/Grid/Events.vue
@@ -3,7 +3,7 @@
         <p class="mb-6 text-4xl font-bold text-[#50FA7B]">Events</p>
         <ul class="lg:text-3xl">
             <li class="mb-4 flex gap-4" v-for="$event in events">
-                <span class="w-64 border-r-4 border-[#50FA7B] pr-2 font-bold">{{
+                <span class="w-40 border-r-4 border-[#50FA7B] pr-2">{{
                     $event.date
                 }}</span>
                 <span>{{ $event.name }}</span>
@@ -18,8 +18,8 @@ type $event = {
     date: string
 }
 const events = ref<$event[]>([
-    { name: 'Mission Team Meeting', date: '10am Sat 20th' },
-    { name: 'Cambodia Info Session', date: '11am Sun 21st' },
-    { name: 'Roy Kim Party (Abi)', date: '1:45pm Sun 28th' },
+    { name: 'Mission Team Meeting', date: 'Sat 20th ' },
+    { name: 'Cambodia Info Session', date: 'Sun 21st' },
+    { name: 'Roy Kim Party (Abi)', date: 'Sun 28th' },
 ])
 </script>
