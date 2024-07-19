a06a6d73b26e9c66444886b9dd7a4304b62546db
Jonathon Choo <jonno.choo@gmail.com>
2024-07-19 21:02:43 +1000

    feat: change to be client only for bible

--- components/Grid/Bible.vue
+++ components/Grid/Bible.vue
@@ -3,22 +3,24 @@
         <div v-if="error">Error</div>
         <div v-else-if="pending"><GridPending /></div>
         <div v-else>
-            <p
-                v-if="error"
-                class="border-l-4 border-[#ffd38f] pl-4 text-2xl lg:text-4xl"
-            >
-                For in Christ all the fullness of the Deity lives in bodily
-                form, and in Christ you have been brought to fullness. He is the
-                head over every power and authority.
-                <span class="mt-4 block font-bold">Colossians 2:9-10</span>
-            </p>
-            <p
-                v-else
-                class="border-l-4 border-[#ffd38f] pl-4 text-2xl lg:text-4xl"
-            >
-                {{ data.text }}
-                <span class="mt-4 block font-bold">{{ data.verse }}</span>
-            </p>
+            <ClientOnly>
+                <p
+                    v-if="error"
+                    class="border-l-4 border-[#ffd38f] pl-4 text-2xl lg:text-4xl"
+                >
+                    For in Christ all the fullness of the Deity lives in bodily
+                    form, and in Christ you have been brought to fullness. He is
+                    the head over every power and authority.
+                    <span class="mt-4 block font-bold">Colossians 2:9-10</span>
+                </p>
+                <p
+                    v-else
+                    class="border-l-4 border-[#ffd38f] pl-4 text-2xl lg:text-4xl"
+                >
+                    {{ data.text }}
+                    <span class="mt-4 block font-bold">{{ data.verse }}</span>
+                </p>
+            </ClientOnly>
         </div>
     </DashGrid>
 </template>
