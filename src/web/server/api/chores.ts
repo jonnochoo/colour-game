a3b3ade8eb645375c563b95a6514742d30a470a9
Jonathon Choo <jonno.choo@gmail.com>
2024-11-12 20:51:52 +1100

    fix: broken build

--- src/web/server/api/chores.ts
+++ src/web/server/api/chores.ts
@@ -38,7 +38,7 @@ export default defineEventHandler(async (event) => {
                 createDailyMorningChores('☀️Sunscreen'),
                 createDailyMorningChores('Put on watch'),
                 createDailyMorningChores('🔍Find hat, jacket, socks & shoes'),
-                createMorningWeekly('Pack swimming bag']),
+                createMorningWeekly('Pack swimming bag', ['Friday']),
 
                 createMorningWeekDayChore('Pack school bag'),
 
@@ -100,7 +100,7 @@ export default defineEventHandler(async (event) => {
                     'Saturday',
                 ]),
                 createMorningWeekly('Pack chinese bag', ['Wednesday']),
-                createMorningWeekly('Pack swimming bag', ['Friday']),
+                createMorningWeekly('Pack swimming bag', ['Friday', 'Tuesday']),
                 createMorningWeekly('Wear sports clothes', [
                     'Tuesday',
                     'Friday',
